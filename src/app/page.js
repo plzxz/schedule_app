d238937e165c3cb43c7badd6ea1ef151b25c8e14
plzxz <144433642+plzxz@users.noreply.github.com>
2025-04-21 12:51:12 +0700

    Add redirect to menu page.

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  return (
-    <main className="flex items-center justify-center h-screen bg-gray-800">
-      <h1 className="text-white text-4xl font-bold">My Schedule (coming soon)</h1>
-    </main>
-  );
+  redirect('/menu');
+  // return (
+  //   <main className="flex items-center justify-center h-screen bg-gray-800">
+  //     <h1 className="text-white text-4xl font-bold">My Schedule (coming soon)</h1>
+  //   </main>
+  // );
 }
